34cf4f0240bd6a9d61df28a4732565949b4a5c26
Kaivalya Mendki <kaivalya.mendki@thoughtworks.com>
2020-08-23 17:08:32 +0530

    Add. Tail-recursive implementation for factorial

--- tail-recursion/index.js
+++ tail-recursion/index.js
@@ -3,7 +3,11 @@ const factorial = (num) => {
     if(num === 0) {
         return 1;
     }
-    return num * factorial(num -1); // More work left to do after recursive call
+    /* More work left to do after recursive call, so JS Engine will need to store frame for each function call
+    * And if very large number is given, it will lead to stack-overflow
+    * Stacktrace remains intact
+     */
+    return num * factorial(num -1);
 }
 console.log('\nNon Tail-Recursive Implementation:')
 console.table([
@@ -18,6 +22,33 @@ console.table([
     {
         number: 1000,
         factorial: factorial(1000)
+    }
+]);
+
+// Tail-recursive function
+const tailRecursiveFactorial = (num) => {
+    if(num === 0) {
+        return 1;
+    }
+    /* No more work left to do after recursive call, so JS Engine will not store frame for each function call
+    * Hence, will never lead to stack-overflow
+    * But the con is, here we will lose the stacktrace
+     */
+    return num * tailRecursiveFactorial(num -1);
+}
+console.log('\nNon Tail-Recursive Implementation:')
+console.table([
+    {
+        number: 4,
+        factorial: tailRecursiveFactorial(4)
+    },
+    {
+        number: 10,
+        factorial: tailRecursiveFactorial(10)
+    },
+    {
+        number: 1000,
+        factorial: tailRecursiveFactorial(1000)
     }
 ]);
 
