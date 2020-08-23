451027dfa5e003b6a0c0bdd052b428132207c893
Kaivalya Mendki <kaivalya.mendki@thoughtworks.com>
2020-08-23 16:25:46 +0530

    Add. Currying using function closures

--- currying/index.js
+++ currying/index.js
@@ -1,22 +1,48 @@
-const multiply = (num1, num2) => {
+const multiplyUsingBind = (num1, num2) => {
     return num1 * num2;
 }
 
 // Method-1: Currying Using "bind"
-const multiplyByTwo = multiply.bind(this, 2);
-const twoMultiplyByThree = multiplyByTwo(3);
-const twoMultiplyByFive = multiplyByTwo(5);
-const twoMultiplyByTwelve = multiplyByTwo(12);
-console.log('Method-1: Currying Using "bind"\n');
+const multiplyByTwo = multiplyUsingBind.bind(this, 2);
+const twoMultipliedByThree = multiplyByTwo(3);
+const twoMultipliedByFive = multiplyByTwo(5);
+const twoMultipliedByTwelve = multiplyByTwo(12);
+
+console.log('\nMethod-1: Currying Using "bind"\n');
 console.table([
     {
         expression: '2 * 3',
-        result: twoMultiplyByThree
+        result: twoMultipliedByThree
     }, {
         expression: '2 * 5',
-        result: twoMultiplyByFive
+        result: twoMultipliedByFive
     }, {
         expression: '2 * 12',
-        result: twoMultiplyByTwelve
+        result: twoMultipliedByTwelve
+    }
+]);
+
+// Method-2: Currying Using function closures
+const multiplyUsingClosure = (num1) => {
+    return (num2) => {
+        return num1 * num2;
+    }
+}
+const multiplyByFour = multiplyUsingClosure(4);
+const fourMultipliedByFive = multiplyByFour(5);
+const fourMultipliedBySeven = multiplyByFour(7);
+const fourMultipliedByTen = multiplyByFour(10);
+
+console.log('\nMethod-2: Currying Using "function closures"');
+console.table([
+    {
+        expression: '4 * 5',
+        result: fourMultipliedByFive
+    }, {
+        expression: '4 * 7',
+        result: fourMultipliedBySeven
+    }, {
+        expression: '4 * 10',
+        result: fourMultipliedByTen
     }
 ]);
